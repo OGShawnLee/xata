0754183a7055f3363b64ebd92cfa37ae9b737c47
Shawn Lee <changoloco.evg@gmail.com>
2023-02-22 20:47:12 -0600

    feat(ui): style scrollbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,18 @@
 </script>
 
 <slot />
+
+<style>
+	:global(::-webkit-scrollbar) {
+		background-color: hsl(240, 8%, 14%);
+		width: 0.75vw;
+	}
+
+	:global(::-webkit-scrollbar-thumb) {
+		background-color: hsl(240, 12%, 30%);
+	}
+
+	:global(::-webkit-scrollbar-thumb:hover) {
+		background-color: hsl(240, 15%, 40%);
+	}
+</style>
