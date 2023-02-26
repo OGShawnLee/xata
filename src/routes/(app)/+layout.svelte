4cfd7568317de907b56a65345d69540767238f44
Shawn Lee <changoloco.evg@gmail.com>
2023-02-26 15:43:39 -0600

    fix(ui): turn sidebar sticky

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -8,8 +8,8 @@
 	currentUser.set(data.user);
 </script>
 
-<div class="max-w-6xl min-h-screen mx-auto py-8 | grid grid-cols-12 items-start gap-8">
-	<Sidebar class="col-span-3 pr-8" user={data.user} />
+<div class="relative max-w-6xl min-h-screen mx-auto py-8 | grid grid-cols-12 items-start gap-8">
+	<Sidebar class="sticky top-8 col-span-3 pr-8" user={data.user} />
 	<main class="col-span-6 | grid gap-8">
 		<slot />
 	</main>
