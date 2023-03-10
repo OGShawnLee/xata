6f859646c36df807cce28ec47415a1c52c78f293
Shawn Lee <changoloco.evg@gmail.com>
2023-03-09 19:42:40 -0600

    fix: redirect to sign-up page properly

--- src/routes/(app)/i/notifications/+page.server.ts
+++ src/routes/(app)/i/notifications/+page.server.ts
@@ -3,7 +3,7 @@ import { error, redirect } from "@sveltejs/kit";
 import { getNotifications } from "$lib/server/notification";
 
 export const load: PageServerLoad = async ({ locals: { user } }) => {
-	if (user.isAnonymous) throw redirect(303, "/home/sign-in");
+	if (user.isAnonymous) throw redirect(303, "/auth/sign-in");
 	const notifications = await getNotifications(user.data.id);
 	if (notifications.failed) throw error(500, { message: "Unable to get notifications." });
 	return { user: user.data, notifications: notifications.data };
