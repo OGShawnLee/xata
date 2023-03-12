fe0a0ec37965730ea078081ac9fd5def58de570b
Shawn Lee <changoloco.evg@gmail.com>
2023-03-11 20:22:25 -0600

    feat(ui): make tweet badge more subtle

--- src/lib/components/Tweet/Badge.svelte
+++ src/lib/components/Tweet/Badge.svelte
@@ -5,10 +5,10 @@
 	const tweet = Context.getContext();
 </script>
 
-<div class="flex items-center gap-1.75 text-zinc-500">
-	<svelte:component this={Repeat} size={16} />
-	<span class="text-sm">
-		<a class="font-medium hover:underline focus:underline" href="/{$tweet.user.displayName}">
+<div class="flex items-center gap-1 | text-zinc-500">
+	<svelte:component this={Repeat} size={14} />
+	<span class="text-xs">
+		<a class="font-semibold hover:underline focus:underline" href="/{$tweet.user.displayName}">
 			{$tweet.user.name}
 		</a>
 		retweeted
