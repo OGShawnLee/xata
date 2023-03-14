17d478f25ff01417acce5b8de0bafdcd100ffbe0
OGShawnLee <changoloco.evg@gmail.com>
2023-03-14 17:08:03 +0000

    fix(ui): do not throw from dialog-compose reply component

--- src/lib/components/Tweet/Menu/Tweet.svelte
+++ src/lib/components/Tweet/Menu/Tweet.svelte
@@ -14,22 +14,19 @@
 		base: "w-full px-4 py-2 | flex items-center gap-4.5",
 		active: { off: "text-zinc-100", on: "bg-zinc-700 text-white" }
 	});
-	const tweet = Context.getContext();
-
-	$: ({ id, user, retweetOf } = $tweet);
+	const tweet = Context.getContext(false);
 </script>
 
-{#if user.id === $currentUser?.id && isNullish(retweetOf)}
+{#if tweet && $tweet.user.id === $currentUser?.id && isNullish($tweet.retweetOf)}
 	<Menu
 		label="View Tweet Options"
 		counter={false}
 		icon={MoreHorizontal}
 		iconClass="stroke-zinc-500"
-		iconSize={20}
-	>
+		iconSize={20}>
 		<MenuItem as="fragment" let:item let:isActive>
 			<form action="/home?/{isPinned ? 'unpin' : 'pin'}" method="post" use:enhance>
-				<input type="hidden" name="tweet-id" value={id} />
+				<input type="hidden" name="tweet-id" value={$tweet.id} />
 				<button class={className({ isActive })} use:item>
 					<svelte:component this={isPinned ? PinOff : Pin} size={20} />
 					<span> {isPinned ? "Unpin Tweet" : "Pin Tweet"} </span>
