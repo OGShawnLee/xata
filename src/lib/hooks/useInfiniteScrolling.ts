6e937c634cc0236d738a18887b5bacd44fd616b0
OGShawnLee <changoloco.evg@gmail.com>
2023-03-27 19:20:49 +0000

    fix: prevent firing intersect event more often than it should

--- src/lib/hooks/useInfiniteScrolling.ts
+++ src/lib/hooks/useInfiniteScrolling.ts
@@ -2,6 +2,7 @@ import type { Paginated } from "@types";
 import type { Expand } from "malachite-ui/types";
 import { parse } from "devalue";
 import { useAwait } from ".";
+import { onDestroy } from "svelte";
 
 interface Config<T, E = unknown> {
 	endpoint: string;
@@ -14,8 +15,16 @@ export default function useInfiniteScrolling<T, E = unknown>({
 	onError,
 	onSuccess
 }: Expand<Config<T, E>>) {
+	let hasCooldown = false;
+	let timeout: NodeJS.Timeout | undefined;
+
+	onDestroy(() => clearTimeout(timeout));
+
 	return async (page: { cursor: string; more: boolean }) => {
-		if (!page.more) return;
+		if (!page.more || hasCooldown) return;
+
+		hasCooldown = true;
+		timeout = setTimeout(() => (hasCooldown = false), 2500);
 
 		const next = await useAwait<Paginated<T>, E>(async () => {
 			const response = await fetch(endpoint + "?cursor=" + page.cursor, {
