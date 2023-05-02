81895078561c4939b721ef20398eb5c1107f985d
OGShawnLee <changoloco.evg@gmail.com>
2023-05-02 02:04:01 +0000

    fix(ui): improve logo-heading vertical alignment

--- app/src/routes/(app)/+layout.svelte
+++ app/src/routes/(app)/+layout.svelte
@@ -11,7 +11,7 @@
 <DialogWarning />
 
 <div class="relative max-w-4xl min-h-screen mx-auto | grid grid-cols-12 items-start gap-8 | xl:max-w-6xl">
-	<Sidebar class="sticky top-0 min-h-screen py-8 xl:(col-span-3 pr-8)" />
+	<Sidebar class="sticky top-0 min-h-screen pt-10 pb-8 xl:(col-span-3 pr-8)" />
 	<main class="pt-4 | col-span-8 grid gap-8 xl:col-span-6">
 		<slot />
 	</main>
