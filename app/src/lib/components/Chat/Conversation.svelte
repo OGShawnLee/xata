7142ac3e97c45bd47067a3f234e4bd0135abb7d6
OGShawnLee <changoloco.evg@gmail.com>
2023-06-27 19:53:05 +0000

    feat(ui): display messages dates

--- app/src/lib/components/Chat/Conversation.svelte
+++ app/src/lib/components/Chat/Conversation.svelte
@@ -1,18 +1,70 @@
+<script lang="ts" context="module">
+	import type { Message } from "@types";
+	import { isEmpty } from "malachite-ui/predicate";
+
+	interface SeriesOfMessages {
+		id: string;
+		messages: Message[];
+		startedAt: Date;
+		user: string | undefined;
+	}
+
+	function createSeriesOfMessages(messages: Message[]) {
+		if (isEmpty(messages)) return [];
+		const series: SeriesOfMessages[] = [
+			{
+				id: messages[0].id,
+				messages: [],
+				startedAt: messages[0].createdAt,
+				user: messages[0].user
+			}
+		];
+		return messages.reduce((series, message) => {
+			const currentSeries = series[series.length - 1];
+			const currentSeriesSeconds = Math.floor(currentSeries.startedAt.getTime() / 1000);
+			const messageSeconds = Math.floor(message.createdAt.getTime() / 1000);
+			const difference = messageSeconds - currentSeriesSeconds;
+			if (difference > 60 || message.user !== currentSeries.user) {
+				series.push({
+					id: message.id,
+					messages: [message],
+					startedAt: message.createdAt,
+					user: message.user
+				});
+			} else {
+				currentSeries.messages.push(message);
+			}
+			return series;
+		}, series);
+	}
+</script>
+
 <script lang="ts">
 	import ChatMessage from "./Message.svelte";
 	import ChatRecipient from "./Recipient.svelte";
+	import Time from "../Time.svelte";
 	import { chatContext } from "$lib/context";
+	import { currentUser } from "$lib/state";
 
 	export let isChatHead = false;
 
 	const { messages } = chatContext.getContext();
+
+	$: seriesOfMessages = createSeriesOfMessages($messages);
 </script>
 
 <div class="flex flex-col gap-3 {isChatHead ? 'pb-4' : 'pr-4 overflow-y-auto'}">
 	<ChatRecipient {isChatHead} />
-	<ul class="{isChatHead ? 'pl-6' : 'pl-6'} | flex flex-col gap-3" class:pr-3={isChatHead}>
-		{#each $messages as message (message.id)}
-			<ChatMessage lighter {message} />
+	<div class="pl-6 | flex flex-col gap-6" class:pr-3={isChatHead}>
+		{#each seriesOfMessages as { id, messages, startedAt, user } (id)}
+			<div class="grid gap-1.25">
+				<ul class="flex flex-col gap-3">
+					{#each messages as message (message.id)}
+						<ChatMessage lighter {message} />
+					{/each}
+				</ul>
+				<Time isMessage isCurrentUser={user === $currentUser?.id} createdAt={startedAt} />
+			</div>
 		{/each}
-	</ul>
+	</div>
 </div>
--- app/src/lib/components/Time.svelte
+++ app/src/lib/components/Time.svelte
@@ -2,10 +2,23 @@
 	import { getRelativeTime } from "$lib/utils";
 
 	export let createdAt: Date | undefined;
+	export let isMessage = false;
+	export let isCurrentUser = false;
 </script>
 
 {#if createdAt}
-	<time class="text-xs text-zinc-500" datetime={createdAt.toISOString()}>
-		{getRelativeTime(createdAt)}
-	</time>
+	{#if isMessage}
+		{@const formatter = Intl.DateTimeFormat("en", { dateStyle: "medium", timeStyle: "short" })}
+		<time
+			class="{isCurrentUser ? 'ml-auto' : 'mr-auto'} text-xs text-zinc-500"
+			datetime={createdAt.toISOString()}
+		>
+			{formatter.format(createdAt)} ·
+			{getRelativeTime(createdAt)}
+		</time>
+	{:else}
+		<time class="text-xs text-zinc-500" datetime={createdAt.toISOString()}>
+			{getRelativeTime(createdAt)}
+		</time>
+	{/if}
 {/if}
